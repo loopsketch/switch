310b83a142b4228c287e6b077ae9d162a4f40a85
loopsketch <loopsketch@6544d933-5e26-41f1-b2f0-1bd7b5b7622e>
2010-11-09 07:11:08 +0000

    git-svn-id: https://svn.osdn.net/svnroot/switch/switch/trunk@726 6544d933-5e26-41f1-b2f0-1bd7b5b7622e

--- src/svvitch/WebAPI.cpp
+++ src/svvitch/WebAPI.cpp
@@ -72,9 +72,9 @@ void SwitchRequestHandler::doRequest() {
 		} else if (urls[1] == "copy") {
 			copy();
 
-		} else if (urls[1] == "reboot") {
-			svvitch::rebootWindows();
-			sendResponse(HTTPResponse::HTTP_OK, "reboot now");
+		//} else if (urls[1] == "reboot") {
+		//	svvitch::rebootWindows();
+		//	sendResponse(HTTPResponse::HTTP_OK, "reboot now");
 
 		} else if (urls[1] == "version") {
 			version();
