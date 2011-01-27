902e9e0a63d1a7884ec586744887518eb6d6b34f
loopsketch <loopsketch@6544d933-5e26-41f1-b2f0-1bd7b5b7622e>
2011-01-27 09:02:43 +0000

    git-svn-id: https://svn.osdn.net/svnroot/switch/switch/trunk@772 6544d933-5e26-41f1-b2f0-1bd7b5b7622e

--- src/svvitch/Renderer.cpp
+++ src/svvitch/Renderer.cpp
@@ -714,8 +714,11 @@ void Renderer::renderScene(const bool visibled, const LONGLONG current) {
 	} else {
 		if (_device->Present(0, 0, 0, 0) == D3DERR_DEVICELOST) {
 			_log.warning("failed device lost");
-//			hr = _device->Reset(&_presentParams[0]);
-//			if FAILED(hr) _log.warning("failed reset device");
+			if (_device->TestCooperativeLevel() == D3DERR_DEVICENOTRESET) {
+				hr = _device->Reset(&_presentParams[0]);
+				if FAILED(hr) _log.warning("failed reset device");
+			}
+
 		} else {
 			if (_captureTexture) {
 				LPDIRECT3DSURFACE9 backBuffer = NULL;
