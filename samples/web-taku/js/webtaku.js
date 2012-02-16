6071b8046e1a3b4e8308456a8c1b8323eeb30ff8
loopsketch <loopsketch@6544d933-5e26-41f1-b2f0-1bd7b5b7622e>
2012-02-16 06:35:59 +0000

    git-svn-id: https://svn.osdn.net/svnroot/switch/switch/trunk@896 6544d933-5e26-41f1-b2f0-1bd7b5b7622e

--- samples/web-taku/js/webtaku.js
+++ samples/web-taku/js/webtaku.js
@@ -1,69 +1,139 @@
 var display = new Array();
-display.push("127.0.0.1");
+display.push("192.168.1.50");
 var root = "http://" + display[0] + ":9090/0/";
 
+var _workspace;
+
 $(document).ready(function() {
-	$("#error").hide();
-	$("#message").hide();
-	$.ajax({
-		type: "GET",
-		url: root + "get/playlist",
-		dataType: "jsonp",
-		cache: false,
-		async: false,
-		success: function(data) {
-			$.each(data["playlists"], function() {
-				var id = this["id"];
-				var item = $("<div>").addClass("playlist-item");
-				item.attr("playlist-id", this["id"]);
-				item.text(this["name"]);
-				item.click(function() {
-					setPlaylist(id);
-					$("div[playlist-id]").removeClass("playlist-item-selected");
-					item.addClass("playlist-item-selected");
-				});
-				$("#playlist-selector").append(item);
-			});
+	$('#error').hide();
+	$('#message').hide();
+	$('#playlist-selector').selectable({
+		autoRefresh: true,
+		tolerance: 'fit',
+		stop: function() {
+			setPlaylist($('.ui-selected').attr('playlist-id'));
 		}
 	});
 
-	$("#doSwitch").click(function() {
+	$('#doSwitch').click(function() {
 		switchContent();
 	});
 
 	getWorkspace();
 });
 
-function message(text) {
-	$("#error").hide();
-	$("#message").text(text).fadeIn(1000);
-}
 
+/** ワークスペース取得 */
 function getWorkspace() {
 	$.ajax({
-		type: "GET",
-		url: root + "download?path=workspace.xml",
-		dataType: "text",
+		type: 'GET',
+		url: root + 'download?path=workspace.xml',
+		dataType: 'xml',
 		cache: false,
 		async: true,
 		success: function(data) {
-			alert(data);
-			message('操作開始できます.\n' + data);
-
-			$("#preview").load(function() {
-				setTimeout(function() {
-					updatePreview();
-				}, 2000);
-			});
-			updatePreview();
+			_workspace = data;
+			setupPlaylist();
+			setupPreview();
+			getStatus();
+			message('操作開始できます.');
+		},
+		error: function(request, status, ex) {
+			message('failed not update workspace: ' + status);
 		}
 	});
 }
 
-function updatePreview() {
-	$('#preview').attr('src', root + 'get/snapshot');
+/** プレイリストのセットアップ */
+function setupPlaylist() {
+	$(_workspace).find('playlist').each(function() {
+		var item = $('<div>').addClass('ui-widget-content');
+
+		item.attr('playlist-id', $(this).attr('id'));
+		item.text($(this).attr('name'));
+		//item.click(function() {
+		//	var id = $(this).attr('playlist-id');
+		//	setPlaylist(id);
+		//});
+		//$(item).bind('selectstart', function() {return false;});
+		$('#playlist-selector').append(item);
+		//console.log($(this).attr('name'));
+	});
 }
 
+/** プレビュー更新のセットアップ */
+function setupPreview() {
+	$('#preview').load(function() {
+		setTimeout(updatePreview, 4000);
+	});
+	updatePreview();
+}
+
+//{"action":"","brightness":"100","current-content":"NHK進化論","current-content-id":"m00012","current-index":"4","current-playlist":"通常リスト","current-playlist-id":"game","next-content":"cristmas tree","next-content-id":"m00030","next-playlist":"通常リスト","next-playlist-id":"game","prepared-content-id":"m00032","stage-description":"","stage-name":"entrance","time_current":"00:01:19.23","time_remain":"00:01:10.22","transition":"","workspace":"15d9129e93a904875c0958cd56cec452"}
+
+/** プレビュー更新 */
+function updatePreview() {
+	$('#preview').attr('src', root + 'get/snapshot?t=' + new Date().getTime());
+}
+
+/** プレビュー更新のセットアップ */
+function getStatus() {
+	$.ajax({
+		type: 'GET',
+		url: root + 'get/status',
+		dataType: 'jsonp',
+		cache: false,
+		async: true,
+		success: function(data) {
+			$('#current-playlist').text(data['current-playlist']);
+			$('#current-content').text(data['current-content']);
+			$('#next-playlist').text(data['next-playlist']);
+			var next = data['next-content'];
+			$('#next-content').text(next);
+			$('#prepared-playlist').text(data['prepared-playlist']);
+			var prepared = data['prepared-content'];
+			if (prepared) {
+				$('#prepared-content').text(prepared);
+			} else {
+				$('#prepared-playlist').text('');
+				$('#prepared-content').text('');
+			}
+			if (next || prepared) {
+				$('#doSwitch').removeAttr("disabled");
+			} else {
+				$('#doSwitch').attr("disabled", "disabled");
+			}
+			setTimeout(getStatus, 500);
+		},
+		error: function(request, status, ex) {
+			message('エラー発生: ' + status);
+		}
+	});
+}
+
+/** プレイリスト準備 */
+function setPlaylist(id) {
+	$.ajax({
+		type: "GET",
+		url: root + "set/playlist",
+		data: {pl: id},
+		dataType: "jsonp",
+		cache: false,
+		async: true,
+		success: function(data) {
+			if (data['playlist']) {
+				message('プレイリストを準備.');
+			} else {
+				error('プレイリストを準備できません.');
+			}
+		},
+		error: function(request, status, ex) {
+			error('エラー発生: ' + status);
+		}
+	});
+}
+
+/** 切替 */
 function switchContent() {
 	$.ajax({
 		type: "GET",
@@ -71,38 +141,26 @@ function switchContent() {
 		dataType: "jsonp",
 		success: function(data) {
 			if (data["switched"]) {
-				$("#error").hide();
-				$("#message").text('切替えました.').fadeIn(1000);
+				message('切替えました.');
 			} else {
-				$("#message").hide();
-				$("#error").text('切替えられません.').fadeIn(1000);
+				error('切替えられません.');
 			}
 		},
 		error: function(request, status, ex) {
-			$("#message").hide();
-			$("#error").text("エラー発生: " + status).fadeIn(1000);
+			error('エラー発生: ' + status);
 		}
 	});
 }
 
-function setPlaylist(id) {
-	$.ajax({
-		type: "GET",
-		url: root + "set/playlist",
-		data: {pl: id},
-		dataType: "jsonp",
-		success: function(data) {
-			if (data["playlist"]) {
-				$("#error").hide();
-				$("#message").text(data["name"] + "のプレイリストを準備しました.").fadeIn(1000);
-			} else {
-				$("#message").hide();
-				$("#error").text('プレイリストを準備できません.').fadeIn(1000);
-			}
-		},
-		error: function(request, status, ex) {
-			$("#message").hide();
-			$("#error").text("エラー発生: " + status).fadeIn(1000);
-		}
-	});
+
+/** */
+function message(text) {
+	$('#error').hide();
+	$('#message').text(text).fadeIn(1000);
+}
+
+/** */
+function error(text) {
+	$('#message').hide();
+	$('#error').text(text).fadeIn(1000);
 }
